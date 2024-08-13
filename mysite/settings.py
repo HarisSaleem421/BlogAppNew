57aea791af905cb9a8ee4730a186392ba97accd9
HarisSaleem421 <harissaleem9421@gmail.com>
2024-08-13 14:13:20 +0500

    Allowed Hosts Updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.25.228.97']
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
